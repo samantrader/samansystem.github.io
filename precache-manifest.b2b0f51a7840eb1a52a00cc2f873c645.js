19d66e3bc298f9bf9a476e72fa317a0d874a8177
samantrader <47695380+samantrader@users.noreply.github.com>
2020-03-23 22:10:57 +0430

    Updates

---
+++ precache-manifest.b2b0f51a7840eb1a52a00cc2f873c645.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadd9278360dc323f7d5fa3880800a78",
+    "url": "/saman/index.html"
+  },
+  {
+    "revision": "8660874542e1e0d54738",
+    "url": "/saman/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "41a79e619f4fd39b5f8d",
+    "url": "/saman/static/js/2.9b37069a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saman/static/js/2.9b37069a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8660874542e1e0d54738",
+    "url": "/saman/static/js/main.9a3bf7b2.chunk.js"
+  },
+  {
+    "revision": "cf89d23cc98db1c6f7ba",
+    "url": "/saman/static/js/runtime-main.8936dd8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/saman/static/media/logo.5d5d9eef.svg"
+  }
+]);
